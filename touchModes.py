bdef8a9d71b81c934cfd2a2c8267e115c3d5fe7f
Myron Richard Dennison <dennisonmra@gmail.com>
2020-11-12 15:10:33 +1100

    a few updates

---
+++ touchModes.py
@@ -0,0 +1,17 @@
+# Dictionaries of Touch Modes as part of the SET_CONGFIGURATION payload [WIP]
+
+touchMode = {
+    # TOVIS/NanoTS Touch Modes [TBU]
+    'NanoTS' : {
+        0x03 : 'Single Touch (Mouse)',
+        0x02 : 'Single Touch (Digitizer)',
+        0x01 : 'Multi-touch (Digitizer)'
+    },    
+
+    # Kortek Touch Modes [TBU]
+    'Kortek' : {
+        0x03 : 'Single Touch (Mouse)',
+        0x02 : 'Single Touch (Digitizer)',
+        0x01 : 'Multi-touch (Digitizer)'
+    }
+}
